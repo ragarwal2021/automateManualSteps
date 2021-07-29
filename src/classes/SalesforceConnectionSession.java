f0b97002cbff520ba5696ddab4766da95260ade0
Rahul Agarwal <ragarwal@dminc.com>
2021-07-29 14:59:35 +0530

    Fix issue in build.xml

--- src/classes/SalesforceConnectionSession.java
+++ src/classes/SalesforceConnectionSession.java
@@ -3,12 +3,13 @@ package AutomateManualDeploymentSteps;
 import java.io.IOException;
 import java.util.concurrent.CancellationException;
 
-import com.sforce.soap.metadata.MetadataConnection;
 import com.sforce.soap.partner.Connector;
 import com.sforce.soap.partner.GetUserInfoResult;
-import com.sforce.soap.partner.LoginResult;
 import com.sforce.soap.partner.PartnerConnection;
 import com.sforce.soap.tooling.ToolingConnection;
+import com.sforce.soap.enterprise.EnterpriseConnection;
+import com.sforce.soap.enterprise.LoginResult;
+import com.sforce.soap.metadata.MetadataConnection;
 import com.sforce.ws.ConnectionException;
 import com.sforce.ws.ConnectorConfig;
 
@@ -22,13 +23,13 @@ public class SalesforceConnectionSession {
         try{
             config = new ConnectorConfig();
             if(orgType == "Sandbox"){
-                config.setAuthEndPoint("https://test/salesforce.com/services/Soap/u/42");
+                config.setAuthEndpoint("https://test/salesforce.com/services/Soap/u/42");
             }else{
-                config.setAuthEndPoint("https://login/salesforce.com/services/Soap/u/42");
+                config.setAuthEndpoint("https://login/salesforce.com/services/Soap/u/42");
             }
             
             config.setUsername(usernameInp);
-            config.setPasswrod(passwordInp);
+            config.setPassword(passwordInp);
         }catch(Exception e){
             System.out.println("Issues in Connection Config");
         }
