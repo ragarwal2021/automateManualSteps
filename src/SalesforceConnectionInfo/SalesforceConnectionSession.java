f22d996c80e272c0fe30a579e297e69b8a19f5cc
Rahul Agarwal <ragarwal@dminc.com>
2021-07-29 17:55:26 +0530

    Fix issue in build.xml

--- src/SalesforceConnectionInfo/SalesforceConnectionSession.java
+++ src/SalesforceConnectionInfo/SalesforceConnectionSession.java
@@ -23,9 +23,9 @@ public class SalesforceConnectionSession {
             config = new ConnectorConfig();
             System.out.println("ogType--> " + orgType);
             if(orgType == "Sandbox"){
-                config.setAuthEndpoint("https://test.salesforce.com/services/Soap/u/42/");
+                config.setAuthEndpoint("https://test.salesforce.com/services/Soap/u/42");
             }else{
-                config.setAuthEndpoint("https://login.salesforce.com/services/Soap/u/42/");
+                config.setAuthEndpoint("https://login.salesforce.com/services/Soap/u/42");
             }
             
             config.setUsername(usernameInp);
